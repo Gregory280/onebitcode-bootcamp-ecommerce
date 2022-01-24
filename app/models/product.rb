b683749e052622307c184b1669135727c78c8558
Gregory <gregory280@hotmail.com>
2022-01-24 15:06:05 -0300

    Modulo 6 - Aula 11

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,5 @@
 class Product < ApplicationRecord
-  belongs_to :productable, polymorphic: true
-  has_one_attached :image
+  
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :description, presence: true
   validates :image, presence: true
@@ -8,8 +7,12 @@ class Product < ApplicationRecord
   validates :status, presence: true
   validates :featured, presence: true, if: -> { featured.nil? }
   enum status: { available: 1, unavailable: 2 }
+
   has_many :product_categories, dependent: :destroy
   has_many :categories, through: :product_categories
+  belongs_to :productable, polymorphic: true
+  has_one_attached :image
+  has_many :wish_items
 
   include LikeSearchable
   include Paginatable
